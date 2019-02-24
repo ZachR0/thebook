69b21fc30816a468a50aed57d5308a89c614761f
Zach Rogers <zachrog@gmail.com>
2019-02-24 12:33:39 -0800

    ohai

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,17 @@
+use std::io; //This allows us to get user input
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    //let creates a variable, for example,
+    // let foo = 5; this is immutable
+    // let mut foo = 5; this is mutable
+
+    let mut guess = String::new(); //Create mutable var of empty string type
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
