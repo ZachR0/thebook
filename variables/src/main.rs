21d408a7266d8132ace503211caa21cf21db4bec
Zach Rogers <zachrog@gmail.com>
2019-02-27 18:39:32 -0800

    add examples of const, mut, and shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,20 @@
+//Const must be annotated
+//Naming is all caps with underscores
+//You can also underscore large numbers for readability
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
-    println!("Hello, world!");
+    println!("const MAX POINTS is: {}", MAX_POINTS);
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    //Example of varaible shadowing
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("The value of y is: {}", y);
+
 }
